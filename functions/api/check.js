5c31053a0dfe03735463cb0646b6fb5247ff5f64
sljqcm <156431531+sljqcm@users.noreply.github.com>
2026-04-20 20:08:16 +0800

    Update check.js

--- functions/api/check.js
+++ functions/api/check.js
@@ -1,1 +1,17 @@
+export async function onRequestGet(context) {
+  const { searchParams } = new URL(context.request.url);
+  const id = searchParams.get('id');
+  const env = context.env;
 
+  const response = await fetch(`https://api.replicate.com/v1/predictions/${id}`, {
+    headers: {
+      "Authorization": `Token ${env.REPLICATE_API_TOKEN}`,
+      "Content-Type": "application/json"
+    }
+  });
+
+  const data = await response.json();
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
